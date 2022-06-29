96a0d72bab8372183919541f5de8d156a74a0642
Bárbara Bitarello <bbitarello@brynmawr.edu>
2022-06-28 23:12:22 -0400

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -3,8 +3,8 @@ timestamp <- function(time) format(time, "%Y-%B-%d_%H-%M-%S")
 
 #' Create output file with timestamp
 #' @param infile Name of input file to modify
-#' @example
-#' @export outfile_path("inst/example.vcf")
+#' @example outfile_path("inst/example.vcf")
+#' @export
 outfile_path <- function(infile = "example.vcf") {
 
         sub("\\..*", paste0("_",timestamp(now),".out"), infile)
