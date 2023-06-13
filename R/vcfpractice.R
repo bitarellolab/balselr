001be297c5188db7e720093e709a96d06c754784
daphnehanse11 <128793799+daphnehanse11@users.noreply.github.com>
2023-06-13 10:09:32 -0400

    practice
    practice

---
+++ R/vcfpractice.R
@@ -0,0 +1,6 @@
+library("data.table")
+chrom_21b<-read_vcf("~/Documents/ALL.chr21.shapeit2_integrated_v1a.GRCh38.20181129.phased.vcf")
+
+read_vcf(example_vcf)
+
+parse_vcf("~/Documents/ALL.chr21.shapeit2_integrated_v1a.GRCh38.20181129.phased.vcf")
