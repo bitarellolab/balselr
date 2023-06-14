29fe23590788010696b0fb14775450a46247d1f4
daphnehanse11 <128793799+daphnehanse11@users.noreply.github.com>
2023-06-14 11:10:18 -0400

    Update vcfpractice.R

--- R/vcfpractice.R
+++ R/vcfpractice.R
@@ -1,6 +1,7 @@
 library("data.table")
-chrom_21b<-read_vcf("~/Documents/ALL.chr21.shapeit2_integrated_v1a.GRCh38.20181129.phased.vcf")
 
-read_vcf(example_vcf)
 
-parse_vcf("~/Documents/ALL.chr21.shapeit2_integrated_v1a.GRCh38.20181129.phased.vcf")
+read_vcf("example_vcf")
+
+read_vcf("chrom22.vcf")
+parse_vcf("chrom22.vcf", type = "ncd1")
