ec1a25994f5f6b2167becd3a27576e86e5c59b8e
daphnehanse11 <128793799+daphnehanse11@users.noreply.github.com>
2023-06-21 12:01:41 -0400

    Update vcfpractice.R

--- R/vcfpractice.R
+++ R/vcfpractice.R
@@ -1,18 +1,18 @@
-library("data.table")
+#library("data.table")
 
 
-read_vcf("example_vcf")
+#read_vcf("example_vcf")
 
 
-read_vcf("/Users/dhansell/Documents/GitHub//chrom22.vcf")
-parsed_vcf22<-parse_vcf("/Users/dhansell/Documents/GitHub//chrom22.vcf", type = "ncd1")
-stat22<-ncd1(parsed_vcf22)
+#read_vcf("/Users/dhansell/Documents/GitHub//chrom22.vcf")
+#parsed_vcf22<-parse_vcf("/Users/dhansell/Documents/GitHub//chrom22.vcf", type = "ncd1")
+#stat22<-ncd1(parsed_vcf22)
 
 
 
-read_vcf("/Users/dhansell/Documents/GitHub//chrom13.vcf")
-parsed_vcf13<-parse_vcf("/Users/dhansell/Documents/GitHub//chrom13.vcf", type = "ncd1")
-stat13<-ncd1(parsed_vcf13)
+#read_vcf("/Users/dhansell/Documents/GitHub//chrom13.vcf")
+#parsed_vcf13<-parse_vcf("/Users/dhansell/Documents/GitHub//chrom13.vcf", type = "ncd1")
+#stat13<-ncd1(parsed_vcf13)
 
-devtools::install_github("bitarellolab/balselr")
-library(balselr)
+#devtools::install_github("bitarellolab/balselr")
+#library(balselr)
