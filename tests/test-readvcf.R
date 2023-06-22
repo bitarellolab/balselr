79fc2e3e17f705cc4bc54b4923141b1be44290bd
daphnehanse11 <128793799+daphnehanse11@users.noreply.github.com>
2023-06-22 12:31:03 -0400

    writing tests

--- tests/test-readvcf.R
+++ tests/test-readvcf.R
@@ -1,2 +1,8 @@
 #the output should be a data.table
-#the output should
+
+testthat::test_that("output is a data table", {
+        readinvcf <-  read_vcf(x="inst/example.vcf")
+        testthat::expect_type(readinvcf, "data.frame")
+})
+
+
