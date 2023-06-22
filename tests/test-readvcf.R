165b56496a36a7415abbfd33ff09477ef51d481c
daphnehanse11 <128793799+daphnehanse11@users.noreply.github.com>
2023-06-22 12:41:04 -0400

    more tests

--- tests/test-readvcf.R
+++ tests/test-readvcf.R
@@ -1,8 +1,11 @@
 #the output should be a data.table
 
 testthat::test_that("output is a data table", {
-        readinvcf <-  read_vcf(x="inst/example.vcf")
+        readinvcf <-  read_vcf(x="inst/example.vcf", only.bi = TRUE)
         testthat::expect_type(readinvcf, "data.frame")
 })
 
-
+testthat::test_that("read_vcf correctly renames column", {
+        readinvcf <- read_vcf(vcf_file_path, only.bi = TRUE)
+        testthat::expect_true("CHR" %in% colnames(readinvcf))
+})
