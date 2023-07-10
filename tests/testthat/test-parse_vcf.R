43c56a345582e2c24051380b5026d6dd8aca5951
daphnehanse11 <128793799+daphnehanse11@users.noreply.github.com>
2023-07-10 14:25:03 -0400

    testing

--- tests/testthat/test-parse_vcf.R
+++ tests/testthat/test-parse_vcf.R
@@ -3,18 +3,13 @@ test_that("Test error is thrown when file doesn't exist", {
                      "VCF file filethatdoesnotexist.vcf does not exist.")
 })
 
-test_that("Test error is thrown when type is not specified", {
-        expect_error(parse_vcf(infile = "example.vcf", n0 = 108, n1 = 1),
-                     "You must choose either 'ncd1' or 'ncd2'")
+
+testthat::test_that("parse_vcf handles invalid inputs", {
+        expect_error(parse_vcf(infile = "non-existent-file", n0 = 108, type = "ncd1"))
+        expect_error(parse_vcf(infile = "testvcf", n0 = 108, type = "invalid_type"))
+        expect_error(parse_vcf(infile = "testvcf", n0 = 108, type = "ncd2"))
+        expect_error(parse_vcf(infile = "testvcf", n0 = a, type = "ncd1"))
 })
 
-test_that("Test error is thrown when n1 is NULL for ncd2 type", {
-        expect_error(parse_vcf(infile = "example.vcf", n0 = 108, type = "ncd2"),
-                     "n1 cannot be NULL. NCD2 requires an outgroup.")
-})
 
-test_that("Test error is thrown when fold is F for ncd1 type", {
-        expect_error(parse_vcf(infile = "example.vcf", n0 = 108, n1 = 1, type = "ncd1", fold = F),
-                     "Only the folded option is available when Ancestral/Derived states are unknown.")
-})
 
--- tests/testthat/test-read_vcf.R
+++ tests/testthat/test-read_vcf.R
@@ -4,13 +4,7 @@ testthat::test_that("output is a data table", {
 })
 
 testthat::test_that("read_vcf correctly renames column", {
-        result <- read_vcf(x="example.vcf", only.bi = TRUE)
+        result <- read_vcf(x="example.vcf")
         expect_true("CHR" %in% colnames(result))
-})
-
-test_that("read_vcf correctly filters data with only.bi parameter", {
-        result_bi <- read_vcf(x="example.vcf", only.bi = TRUE)
-        result_all <- read_vcf(x="example.vcf", only.bi = FALSE)
-        expect_true(nrow(result_bi) <= nrow(result_all))
 })
 
