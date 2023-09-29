714c0c83f71d8b68904b1d22433a97e64c4165d7
daphnehanse11 <128793799+daphnehanse11@users.noreply.github.com>
2023-09-29 10:46:27 -0400

    it works now!
    i added the argument to filter by person to read.vcf

--- R/modified_read_vcf.R
+++ R/modified_read_vcf.R
@@ -11,19 +11,34 @@
 #' @importFrom data.table ":="
 #'
 mod_read_vcf <- function(x = "inst/example.vcf", id.range = NULL) {
-    inp <- data.table::fread(x, skip = "##", header = T)
-    data.table::setnames(inp, "#CHROM", "CHR")
+        # Read VCF file
+        inp <- data.table::fread(x, skip = "##", header = TRUE)
+        data.table::setnames(inp, "#CHROM", "CHR")
 
-    inp <- inp[REF %in% c("A", "C", "T", "G")][ALT %in% c("A", "C", "T", "G")]
+        inp <- inp[REF %in% c("A", "C", "T", "G") & ALT %in% c("A", "C", "T", "G")]
+        if (!is.null(id.range)) {
+                if (length(id.range) != 1 && length(id.range) != 2) {
+                        stop("id.range must have either 1 or 2 values.")
+                }
 
-    if (!is.null(id.range)) {
-        if (length(id.range) == 2) {
-            cols_to_keep <- c("CHR", "POS", "ID", "REF", "ALT", (6 + id.range[1]):(5 + id.range[2]))
-        } else {
-            cols_to_keep <- c("CHR", "POS", "ID", "REF", "ALT", 5 + id.range)
+                if (length(id.range) == 2 && id.range[1] > id.range[2]) {
+                        stop("The first value of id.range must be less than or equal to the second value.")
+                }
+
+                basic_cols <- c("CHR", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO", "FORMAT")
+
+                if (length(id.range) == 2) {
+                        sample_cols <- paste0("i", (id.range[1]-1):(id.range[2]-1))
+                } else {
+                        sample_cols <- paste0("i", id.range-1)
+                }
+
+                if (!all(sample_cols %in% names(inp))) {
+                        stop("Some specified sample columns do not exist in the VCF file.")
+                }
+
+                inp <- inp[, c(basic_cols, sample_cols), with = FALSE]
         }
-        inp <- inp[, ..cols_to_keep]
-    }
 
-    return(inp)
+        return(inp)
 }
