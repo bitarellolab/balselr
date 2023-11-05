0dbcd89e0bbe52d6a7fa361ceb44366aca1461ab
daphnehanse11 <128793799+daphnehanse11@users.noreply.github.com>
2023-11-05 13:28:09 -0500

    FINALLY
    this took so long to make this work omg. it finally filters properly

--- R/modified_read_vcf.R
+++ R/modified_read_vcf.R
@@ -1,8 +1,6 @@
-
 #' Read vcf
 #'
 #' @param x The path and name of a vcf file
-#' @param id.range A numeric vector specifying the IDs or range of IDs to keep.
 #' @return Returns a data.table object containing only SNPs
 #' @export
 #'
@@ -10,41 +8,32 @@
 #' @import data.table
 #' @importFrom data.table ":="
 #'
-mod_read_vcf <- function(x = "inst/example.vcf", id.range = NULL, pos.range = NULL) {
+modmod_read_vcf <- function(x = "inst/example.vcf", pos.range = NULL, id.range = NULL) {
         inp <- data.table::fread(x, skip = "##", header = TRUE)
         data.table::setnames(inp, "#CHROM", "CHR")
 
         inp <- inp[REF %in% c("A", "C", "T", "G") & ALT %in% c("A", "C", "T", "G")]
+
         if (!is.null(pos.range)) {
-                if (length(pos.range) != 2 || pos.range[1] > pos.range[2]) {
-                        stop("pos.range must have 2 values with the first being less than or equal to the second.")
-                }
-
-                inp <- inp[POS >= pos.range[1] & POS <= pos.range[2]]
-        }
-        if (!is.null(id.range)) {
-                if (length(id.range) != 1 && length(id.range) != 2) {
-                        stop("id.range must have either 1 or 2 values.")
-                }
-
-                if (length(id.range) == 2 && id.range[1] > id.range[2]) {
-                        stop("The first value of id.range must be less than or equal to the second value.")
-                }
-
-                basic_cols <- c("CHR", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO", "FORMAT")
-
-                if (length(id.range) == 2) {
-                        sample_cols <- paste0("i", (id.range[1]-1):(id.range[2]-1))
+                if (length(pos.range) == 2) {
+                        inp <- inp[POS >= pos.range[1] & POS <= pos.range[2]]
                 } else {
-                        sample_cols <- paste0("i", id.range-1)
+                        inp <- inp[POS %in% pos.range]
                 }
-
-                if (!all(sample_cols %in% names(inp))) {
-                        stop("Some specified sample columns do not exist in the VCF file.")
-                }
-
-                inp <- inp[, c(basic_cols, sample_cols), with = FALSE]
         }
+
+        col.range <- 1:9
+
+        if (!is.null(id.range)) {
+                if (length(id.range) == 2) {
+                        col.range <- c(col.range, (10 + id.range[1] - 1):(10 + id.range[2] - 1))
+                } else {
+                        col.range <- c(col.range, 9 + id.range)
+                }
+                col.range <- col.range[col.range <= ncol(inp)]
+        }
+
+        inp <- inp[, .SD, .SDcols = col.range]
 
         return(inp)
 }
