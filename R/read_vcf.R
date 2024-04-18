41f2fdea7cfadc0e2120e47dc6a9272d8286adb7
Bárbara Bitarello <barbarabitarello@gmail.com>
2024-04-18 14:18:10 -0400

    updt

--- R/read_vcf.R
+++ R/read_vcf.R
@@ -8,9 +8,32 @@
 #' @import data.table
 #' @importFrom data.table ":="
 #'
-read_vcf <- function(x = "inst/example.vcf") {
-        inp <- data.table::fread(x, skip = "##", header = T)
+read_vcf <- function(x = "inst/example.vcf", pos.range = NULL, id.range = NULL) {
+        inp <- data.table::fread(x, skip = "##", header = TRUE)
         data.table::setnames(inp, "#CHROM", "CHR")
-        #inp <-
-        inp[REF %in% c("A", "C", "T", "G")][ALT %in% c("A", "C", "T", "G")]
+
+        inp <- inp[REF %in% c("A", "C", "T", "G") & ALT %in% c("A", "C", "T", "G")]
+
+        if (!is.null(pos.range)) {
+                if (length(pos.range) == 2) {
+                        inp <- inp[POS >= pos.range[1] & POS <= pos.range[2]]
+                } else {
+                        inp <- inp[POS %in% pos.range]
+                }
+        }
+
+        col.range <- 1:9
+
+        if (!is.null(id.range)) {
+                if (length(id.range) == 2) {
+                        col.range <- c(col.range, (10 + id.range[1] - 1):(10 + id.range[2] - 1))
+                } else {
+                        col.range <- c(col.range, 9 + id.range)
+                }
+                col.range <- col.range[col.range <= ncol(inp)]
+        }
+
+        inp <- inp[, .SD, .SDcols = col.range]
+
+        return(inp)
 }
