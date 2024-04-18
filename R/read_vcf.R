ba44052bcf25630e7a2f429d916fc4d123a8ff5c
Bárbara Bitarello <barbarabitarello@gmail.com>
2024-04-18 12:44:13 -0400

    asdasa

--- R/read_vcf.R
+++ R/read_vcf.R
@@ -1,25 +1,17 @@
 #' Read vcf
 #'
 #' @param x The path and name of a vcf file
-#' @param only.bi Logical. If TRUE, only bi-allelic SNP positions from VCF file
-#' are kept.
-#' @return Returns a data.table object
+#' @return Returns a data.table object containing only SNPs
 #' @export
 #'
-#' @examples read_vcf(x="inst/example.vcf", only.bi=T")
+#' @examples read_vcf(x=system.file(package="balselr", "example.vcf"))
 #' @import data.table
 #' @importFrom data.table ":="
-read_vcf <- function(x = "inst/example.vcf",
-                     only.bi = T) {
-        REF <- ALT <- x2 <- NULL
+#'
+read_vcf <- function(x = "inst/example.vcf") {
         inp <- data.table::fread(x, skip = "##", header = T)
         data.table::setnames(inp, "#CHROM", "CHR")
-        if (only.bi == T) {
-                inp <-
-                        inp[REF %in% c("A", "C", "T", "G")][ALT %in% c("A", "C", "T", "G")]
-                inp
-        } else {
-                inp
-        }
+        #inp <-
+        inp[REF %in% c("A", "C", "T", "G")][ALT %in% c("A", "C", "T", "G")]
+        #return(inp)
 }
-
